e9b807633c28d07a9dc46992bbea2eba13bd80e7
Farid Habibli <hebibliferid20@gmail.com>
2022-11-22 16:50:50 +0400

    sdfd

---
+++ main.js
@@ -0,0 +1,15 @@
+var tiger1 = {
+    id: 6,
+    name: 'tiger1',
+    age: 2,
+    food: 200,
+    animal: "tiger",
+    biome: "tropical",
+    reservoir: false,
+    requiredSpace: 1000,
+    diet: "carnivore",
+    typeofAnimal:"predator",
+}
+
+
+
